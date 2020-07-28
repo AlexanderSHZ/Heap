7afc4936e943b0c1b2b63e21bbd67b1663bb08ec
Alexander SHZ <xxxshzxxx@gmail.com>
2020-07-29 01:12:51 +0300

    rebuild perviouse code

--- Heap.cpp
+++ Heap.cpp
@@ -1,32 +1,43 @@
-﻿#include <string.h>
-#include <iostream>
+﻿#include <iostream>
 #pragma warning(disable : 4996)
 
-char* InpText(char* string);           //Получает строку от пользователя.
-double upper(int i, char* text);       //Приводит к нижнему регистру.
+char* sort(int n, char* textPtr);      //Посимвольно сортирует строку.
+double upper(int i, char* text);       //Приводит к нижнему регистру. Ф-ция для sort.
 
 
 int main(void)
 {
-    char text[80];
     char* textPtrAccessory = NULL;
-    char* textPtr = { 0 };
-    char  tmp;
-    int length = 0;
-  
+    char* textPtr = NULL;
+    int count = 0;
+    int n = 10;
+    char ch = 0;
     
     fflush(stdout);
 
-    textPtr = (char*)malloc(80);                 
-    if (textPtr)
+    textPtr = (char*)malloc(sizeof(char) * n);
+
+    while (((ch = getchar()) != '\n') && (ch != EOF))       //Получение указателя на строку из консоли.
     {
-        sprintf(text, "%s", InpText(textPtr));      //Полученную строку посимвольно сохраняем в массив.
-        length=strlen(text);                      //Получаем количество символов.
+        textPtr[count] = ch;
+        count++;
+
+        if (count > n - 2)
+        {
+            textPtrAccessory = (char*)realloc(textPtrAccessory, sizeof(char) * (count+n));
+            if (textPtrAccessory != NULL)
+            {
+                textPtr = textPtrAccessory;
+            }
+            else
+            {
+                printf("Program operation failed! \n");
+            }
+        }
+        
     }
 
-    textPtr = textPtrAccessory;
-
-    textPtrAccessory = (char*)realloc(textPtrAccessory, sizeof(char) * (length + 1));    //изменяем размер массива под нашу строку.
+    textPtrAccessory = (char*)realloc(textPtrAccessory, sizeof(char) * (count+1));
     if (textPtrAccessory != NULL)
     {
         textPtr = textPtrAccessory;
@@ -35,12 +46,38 @@ int main(void)
     {
         printf("Program operation failed! \n");
     }
-
     free(textPtrAccessory);
+    
+    sort(count, textPtr);                    //Сортируем символы
+                            
+    printf("%s", textPtr);                   //Выводим результат на консоль
+    printf(" \n");
 
-    for (int i = 0; i < length; i++)                            //Посимвольно сортируем строку.
+    free(textPtr);
+    return 0;
+}
+double upper(int i, char* textPtr)   //Приведение к нижнему регистру.
+{
+    double a;
+
+    if (textPtr[i] < 97)
     {
-        for (int j = i + 1; j < length - 1; j++)
+        a = textPtr[i] + 31.5;
+    }
+    else
+    {
+        a = textPtr[i];
+    }
+    return a;
+}
+
+char* sort(int n, char* textPtr)                     //Cортировка массива.
+{
+    char tmp;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n - 1; j++)
         {
 
             if (upper(j, textPtr) < upper(i, textPtr))
@@ -51,43 +88,5 @@ int main(void)
             }
         }
     }
-
-    for (int i = 0; i <= length; i++)                          //Выводим результат на консоль
-        printf("%c", textPtr[i]);
-    printf(" \n");
-
-    free(textPtr);
-    return 0;
+    return textPtr;
 }
-double upper(int i, char* text)   //Приведение к нижнему регистру.
-{
-    double a;
-
-    if (text[i] < 97)
-    {
-        a = text[i] + 31.5;
-    }
-    else
-    {
-        a = text[i];
-    }
-    return a;
-}
-
-char* InpText(char* string)       //Получение указателя на строку из консоли, максимальный размер строки задаётся.
-{
-
-    int ch;
-    int i = 0;
-    int inputlength = 80;
-
-       for (i = 0; i < inputlength - 1 && (((ch = getchar()) != '\n') && (ch != EOF)); i++)
-    {
-        string[i] = ch;
-        
-    }
-    string[i] = '\0';
-    
-    return string;
-}
-
